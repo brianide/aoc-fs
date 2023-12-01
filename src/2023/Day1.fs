f2066fd425ee33dbbd2dafc8724d7b05d8861702
brianide <brianide@gmail.com>
2023-12-01 08:42:28 -0600

    Slightly washed

--- src/2023/Day1.fs
+++ src/2023/Day1.fs
@@ -12,13 +12,13 @@ let solveSilver path =
     |> Seq.sum
     |> string
 
-let (|StartsWith|_|) (prefix: string) (line: string) =
-    if line.StartsWith(prefix) then
-        Some StartsWith
-    else
-        None
-
 let solveGold path =
+    let (|StartsWith|_|) (prefix: string) (line: string) =
+        if line.StartsWith(prefix) then
+            Some StartsWith
+        else
+            None
+
     let rec parseLine (line: string) =
         seq {
             let mutable line = line
@@ -27,26 +27,16 @@ let solveGold path =
                 line <- line.Substring 1
         }
         |> Seq.choose (function
-            | StartsWith "0" -> Some 0
-            | StartsWith "1" -> Some 1
-            | StartsWith "2" -> Some 2
-            | StartsWith "3" -> Some 3
-            | StartsWith "4" -> Some 4
-            | StartsWith "5" -> Some 5
-            | StartsWith "6" -> Some 6
-            | StartsWith "7" -> Some 7
-            | StartsWith "8" -> Some 8
-            | StartsWith "9" -> Some 9
-            | StartsWith "zero" -> Some 0
-            | StartsWith "one" -> Some 1
-            | StartsWith "two" -> Some 2
-            | StartsWith "three" -> Some 3
-            | StartsWith "four" -> Some 4
-            | StartsWith "five" -> Some 5
-            | StartsWith "six" -> Some 6
-            | StartsWith "seven" -> Some 7
-            | StartsWith "eight" -> Some 8
-            | StartsWith "nine" -> Some 9
+            | StartsWith "0" | StartsWith "zero" -> Some 0
+            | StartsWith "1" | StartsWith "one" -> Some 1
+            | StartsWith "2" | StartsWith "two" -> Some 2
+            | StartsWith "3" | StartsWith "three" -> Some 3
+            | StartsWith "4" | StartsWith "four" -> Some 4
+            | StartsWith "5" | StartsWith "five" -> Some 5
+            | StartsWith "6" | StartsWith "six" -> Some 6
+            | StartsWith "7" | StartsWith "seven" -> Some 7
+            | StartsWith "8" | StartsWith "eight" -> Some 8
+            | StartsWith "9" | StartsWith "nine" -> Some 9
             | _ -> None)
 
     File.ReadAllLines path
