f0fcbab0956cb3a3cb0d61707aa20f72f3d6c5d4
brianide <brianide@gmail.com>
2023-12-04 08:41:19 -0600

    Minor refactoring

--- src/2023/Day4.fs
+++ src/2023/Day4.fs
@@ -6,15 +6,15 @@ open Scaffold.Attributes
 open Scaffold.Handlers
 open Scaffold.Extensions
 
-let parse path =
+let parse =
     let cardNum = skipString "Card" >>. spaces1 >>. pint32 .>> skipChar ':' .>> spaces1
     let ws = skipMany (pchar ' ')
     let numList term = many1Till (pint32 .>> ws) term |>> set
-    let line = cardNum .>>. (numList (skipChar '|' .>> spaces1) .>>. numList newline)
+    let line = cardNum >>. (numList (skipChar '|' .>> spaces1) .>>. numList newline)
 
-    File.ReadAllText path
-    |> run (many line)
-    |> function
+    File.ReadAllText
+    >> run (many line)
+    >> function
     | Success (res, _, _) -> res
     | Failure (msg, _, _) -> failwith msg
 
@@ -22,17 +22,19 @@ let countWinning (a, b) =
     Set.intersect a b |> Seq.length
 
 let solveSilver =
-    Seq.sumBy (snd >> countWinning >> (fun n -> n - 1) >> pown 2)
+    Seq.sumBy (countWinning >> (fun n -> n - 1) >> pown 2)
     >> string
 
-let solveGold input =
-    let cards = input |> Seq.map (snd >> countWinning) |> Seq.rev |> Seq.toList
+let solveGold =
     let rec proc prev = function
     | [] -> Seq.sum prev
     | next :: rest -> prev |> Seq.takeUpTo next |> Seq.sum |> ((+) 1) |> fun n -> proc (n :: prev) rest
 
-    proc [] cards
-    |> string
+    Seq.map countWinning
+    >> Seq.rev
+    >> Seq.toList
+    >> proc []
+    >> string
 
 [<Solution("2023", "4", "Scratchcards")>]
 let Solver = chainFileHandler parse solveSilver solveGold
