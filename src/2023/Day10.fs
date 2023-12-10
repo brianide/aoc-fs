50ccabcdd770d568bb1089c09c6221e2bd61de9d
brianide <brianide@gmail.com>
2023-12-10 14:04:43 -0600

    Day 10 part 2

--- src/2023/Day10.fs
+++ src/2023/Day10.fs
@@ -6,6 +6,9 @@ open Scaffold.Handlers
 open Scaffold.Extensions
 open Scaffold.Image
 
+let inline tadd (a, b) (t, u) = (a + t, b + u)
+let inline tmul m (a, b) = (a * m, b * m)
+
 let parse path =
     let charGrid =
         File.ReadAllLines path
@@ -13,72 +16,150 @@ let parse path =
         |> array2D
 
     let rows, cols = Array2D.length1 charGrid, Array2D.length2 charGrid
-    let inline inside (r, c) = 0 <= r && r < rows && 0 <= c && c < cols
-    let inline (.+) (a, b) (t, u) = (a + t, b + u)
-    let offset a offs = offs |> List.map ((.+) a) |> List.filter inside
+    let inline recenter a = a |> tmul 3 |> tadd (1, 1)
+    let offset a offs = offs |> List.map (recenter a |> tadd)
 
     let mutable start = None
     let pipeGrid =
-        charGrid |> Array2D.mapi (fun r c ->
+        ([], charGrid) ||> Array2D.foldi (fun acc r c ->
             function
-            | '|' -> [-1, 0; 1, 0]
-            | '-' -> [0, 1; 0, -1]
-            | 'L' -> [-1, 0; 0, 1]
-            | 'J' -> [-1, 0; 0, -1]
-            | '7' -> [1, 0; 0, -1]
-            | 'F' -> [1, 0; 0, 1]
+            | '|' -> [0, 0; -1, 0; 1,  0]
+            | '-' -> [0, 0;  0, 1; 0, -1]
+            | 'L' -> [0, 0; -1, 0; 0,  1]
+            | 'J' -> [0, 0; -1, 0; 0, -1]
+            | '7' -> [0, 0;  1, 0; 0, -1]
+            | 'F' -> [0, 0;  1, 0; 0,  1]
             | '.' -> []
-            | 'S' -> start <- Some (r, c); [-1, 0; 0, 1; 1, 0; 0, -1]
+            | 'S' -> start <- (r, c) |> recenter |> Some; [0, 0; -1, 0; 0, 1; 1, 0; 0, -1]
             | ch -> failwithf "Unknown pipe char at %d %d: %A" r c ch 
-            >> offset (r, c))
+            >> offset (r, c)
+            >> fun n -> n :: acc)
+        |> List.collect id
+        |> Set.ofList
+        
     
     match start with
-    | Some s -> pipeGrid, s
+    | Some s -> pipeGrid, s, (rows * 3, cols * 3)
     | None -> failwith "No starting position found"
 
-let solveSilver (grid: list<int*int>[,], (sr, sc)) =
-    // Take one of the two neighbors of S which actually connect back to it
-    let init =
-        grid[sr, sc]
-        |> List.filter (fun (r, c) -> List.contains (sr, sc) grid[r, c])
+let getPath (tiles, start, dims) =
+    let dir =
+        [0, -1; 0, 1; -1, 0; 1, 0]
+        |> List.filter (fun p -> Set.contains (p |> tmul 2 |> tadd start) tiles)
         |> List.head
-    
-    let rec travel (r, c) prev count =
-        if (r, c) = (sr, sc) then
-            count
+        |> tadd start
+
+    let rec traverse curr path =
+        if curr = start then
+            path
         else
-            let next = grid[r, c] |> List.filter (fun n -> n <> prev)|> List.exactlyOne
-            travel next (r, c) (count + 1)
-    travel init (sr, sc) 1
-    |> fun n -> n / 2
+            [0, -1; 0, 1; -1, 0; 1, 0]
+            |> List.map (tadd curr)
+            |> List.filter (fun p -> p <> List.head path && Set.contains p tiles)
+            |> List.exactlyOne
+            |> fun p -> traverse p (curr :: path)
+    
+    traverse dir [start]
+    |> List.rev
+    |> fun p -> p, dims
+    
+    // let neighbors p =
+    //     [0, -1; 0, 1; -1, 0; 1, 0]
+    //     |> List.map (tadd p)
+    //     |> List.filter (fun p -> Set.contains p tiles)
+
+    // let rec bfs queue visited depth =
+    //     if List.isEmpty queue then
+    //         depth
+    //     else
+    //         queue
+    //         |> List.collect neighbors
+    //         |> List.filter (fun p -> Set.contains p visited |> not)
+    //         |> fun ps -> ps, Set.union visited (set ps), depth + 1
+    //         |||> bfs
+            
+    // bfs [start] (Set.singleton start) 0
+    // |> fun d -> d / 3
+    // |> sprintf "%d"
+
+let solveSilver (path, _) =
+    List.length path / 6
     |> sprintf "%d"
 
-let solveSilverBFS (grid: list<int*int>[,], (sr, sc)) =
-    // Find the two neighbors of S which actually connect back to it
-    let init =
-        grid[sr, sc]
-        |> List.map (fun (r, c) -> grid[r, c]) |> List.filter (List.contains (sr, sc))
-        |> List.collect id
+type Dir = N | S | E | W
 
-    // Level-by-level BFS
-    let mutable count = 0
-    let rec bfs queue seen depth =
-        // saveToPGM (Array2D.length2 grid) (Array2D.length1 grid) (sprintf "frames/frame_%06d.pgm" count) (fun r c ->
-        //     if Set.contains (r, c) seen then byte 255 else byte 0)
-        // count <- count + 1
+let solveGold (path, (rows, cols)) =
+    let segments = 
+        List.pairwise path
+        |> List.map (fun (a, b) -> b |> tmul -1 |> tadd a)
+        |> List.map (function
+            |  0,  1 -> E
+            |  0, -1 -> W
+            |  1,  0 -> S
+            | -1,  0 -> N
+            | x -> failwithf "Invalid path segment: %A" x)
+    
+    let turns = 
+        List.pairwise segments
+        |> List.map (function
+            | N, E | E, S | S, W | W, N -> 1
+            | N, W | W, S | S, E | E, N -> -1
+            | t, u when t = u -> 0
+            | x -> failwithf "Invalid turn: %A" x)
+        |> List.reduce (+)
+
+    let fillBegin =
+        match turns, (List.item 2 segments) with
+        | t, N when t < 0 ->  0,  1
+        | t, N when t > 0 ->  0, -1
+        | t, S when t < 0 ->  0, -1
+        | t, S when t > 0 ->  0,  1
+        | t, E when t < 0 ->  1,  0
+        | t, E when t > 0 -> -1,  0
+        | t, W when t < 0 -> -1,  0
+        | t, W when t > 0 ->  1,  0
+        | x -> failwithf "Unhandled start case: %A" x
+        |> tadd (List.item 2 path)
+
+    // 2D array to keep track of visited tiles
+    let visited = Array2D.create rows cols false
+    for (r, c) in path do visited[r, c] <- true
+
+    // Floodfill
+    let rec bfs queue depth =
         if List.isEmpty queue then
             depth
         else
-            queue
-            |> List.collect (fun (r, c) -> grid[r, c])
-            |> List.filter (fun n -> Set.contains n seen |> not)
-            |> fun p -> p, Set.union seen (set p), depth + 1
-            |||> bfs
-    bfs init (Set.singleton (sr, sc)) 1
+            // printfn "%A" queue.Length
+            // let queueset = set queue
+            // saveToPGM cols rows (sprintf ".frames/%04d.pgm" depth) (fun r c ->
+            //     if Set.contains (r, c) queueset then
+            //         byte 255
+            //     else if visited[r, c] then
+            //         byte 127
+            //     else
+            //         byte 0)
+            [ for p in queue do
+                for off in [-1, 0; 1, 0; 0, -1; 0, 1] do
+                    let (r, c) = tadd p off
+                    if visited[r, c] = false then (r, c)]
+            |> fun s -> s |> set |> Set.toList
+            |> (fun ps ->
+                for (r, c) in ps do visited[r, c] <- true
+                ps, depth + 1)
+            ||> bfs
+    
+    bfs [fillBegin] 0 |> ignore
+
+    // Remove tiles adjacent to pipes
+    for (r, c) in path do
+        for ro in -1..1 do
+            for co in -1..1 do
+                if ro <> 0 || co <> 0 then
+                    visited[r + ro, c + co] <- false
+
+    Array2D.fold (fun acc v -> if v then acc + 1 else acc) 0 visited / 9
     |> sprintf "%d"
 
-let solveGold input =
-    "Not implemented"
-
 [<Solution("2023", "10", "Pipe Maze")>]
-let Solver = chainFileHandler parse solveSilver solveGold
+let Solver = chainFileHandler (parse >> getPath) solveSilver solveGold
