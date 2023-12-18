fd19943b65ea0a0c9b3de657aa376024fe04840b
brianide <brianide@gmail.com>
2023-12-18 07:08:49 -0600

    Day 18 part 2

--- src/2023/Day18.fs
+++ src/2023/Day18.fs
@@ -1,45 +1,51 @@
 module Year2023.Day18
 
+open System.Globalization
 open System.IO
 open FParsec
 open Scaffold.Parsec
 open Scaffold.Attributes
 open Scaffold.Handlers
 open Scaffold.Extensions
-open Scaffold.Util.Patterns
 
 let parse =
-    let dir = anyOf "LRUD" .>> spaces1
-    let dist = pint32 .>> spaces1
+    let dir = anyOf "RDLU" .>> spaces1
+    let dist = pint64 .>> spaces1
     let color = between (skipChar '(') (skipChar ')') (skipChar '#' >>. many1Satisfy isHex) .>> newline
-    let line = tuple3 dir dist color
-    let spec = many1 line
+
+    let conv color =
+        let hex, dirNum = String.splitAt 5 color
+        let dist = System.Int64.Parse(hex, NumberStyles.HexNumber)
+        let dir = "RDLU"[int dirNum]
+        dir, dist
+
+    let line = pipe3 dir dist color (fun dir dist color -> (dir, dist), conv color)
 
     File.ReadAllText
-    >> getParsed spec 
+    >> getParsed (many1 line)
 
 let inline tmul m (a, b) = a * m, b * m
 let inline tadd (a, b) (x, y) = a + x, b + y
 
-let solveSilver input =
-    let foldCoords (prev, coords, len) (dir, dist, _) =
+let solve input =
+    let foldCoords (prev, coords, len) (dir, dist) =
         let pos =
             match dir with
-            | 'U' -> -dist, 0 | 'D' -> dist, 0 | 'L' -> 0, -dist | 'R' -> 0, dist
+            | 'U' -> -dist, 0L | 'D' -> dist, 0L | 'L' -> 0L, -dist | 'R' -> 0L, dist
             | x -> failwithf "Invalid direction: %A" x
             |> tadd prev
 
         pos, (pos :: coords), len + dist
     
-    let _, coords, length = input |> List.fold foldCoords ((0, 0), [], 0)
+    let _, coords, length = input |> List.fold foldCoords ((0L, 0L), [], 0L)
     
     List.pairwise coords
-    |> List.sumBy (fun ((x1, y1), (x2, y2)) -> (x2 + x1) * (y2 - y1))
-    |> fun n -> (n + length) / 2 + 1
-    |> sprintf "%A"
+    |> List.sumBy (fun ((x1, y1), (x2, y2)) -> x1 * y2 - y1 * x2)
+    |> fun n -> (n + length) / 2L + 1L
+    |> sprintf "%d"
 
-let solveGold input =
-    "Not implemented"
+let solveSilver = List.map fst >> solve
+let solveGold = List.map snd >> solve
 
 [<Solution("2023", "18", "Lavaduct Lagoon")>]
 let Solver = chainFileHandler parse solveSilver solveGold
